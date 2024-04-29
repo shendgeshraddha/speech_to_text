f4c2789891799ddabbc3d88e56a3d9f1dea4d16d
shendgeshraddha <shendgeshraddhs069@gmail.com>
2024-04-30 00:41:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition(); 
+   if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+    }
+    return (
+        <>
+            <div className="container">
+                <h2>Speech To Text Converter</h2>
+                <br/>
+                <p>A react hook that converts speech from the microphone to text and makes it
+                    available to your react components.</p>
+
+                <div className="main-content">
+                    {transcript}
+</div>
+                <div className="btn-style">
+                     <p>Microphone: {listening ? 'on' : 'off'}</p>
+                    <button onClick={SpeechRecognition.startListening}>Start Listening</button>
+                    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+  <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default App;
